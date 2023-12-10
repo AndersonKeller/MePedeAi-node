88b2519a39196bc09f7ae61bdc498f808844beb3
Anderson Keller <andersonkellervendas@hotmail.com>
2023-12-10 19:03:41 -0300

    to vercel build

--- src/data-source.ts
+++ src/data-source.ts
@@ -40,5 +40,5 @@ const dataSourceConfig = (): DataSourceOptions => {
 const AppDataSource = new DataSource(dataSourceConfig());
 //npm run typeorm migration:generate ./src/migrations/InitialMigration -- -d ./src/data-source.ts
 //npm run typeorm migration:run -- -d ./src/data-source
-//npm run typeorm migration:run -- -d ./src/data-source
+//to vercel build.sh
 export { AppDataSource };
